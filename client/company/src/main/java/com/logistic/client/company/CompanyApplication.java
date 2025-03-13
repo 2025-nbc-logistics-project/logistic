1fee628d796447d6ad5eeea0efc76954a50ba9e8
zapzookj <zapzook@naver.com>
2025-03-13 16:18:54 +0900

    init commit

---
+++ client/company/src/main/java/com/logistic/client/company/CompanyApplication.java
@@ -0,0 +1,13 @@
+package com.logistic.client.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyApplication.class, args);
+	}
+
+}
