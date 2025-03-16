ef9263f19d4617d686e124d839b0c9965c4fb452
zapzookj <zapzook@naver.com>
2025-03-16 21:16:19 +0900

    fix : FeignClient 수정

--- client/order/src/main/java/com/logistic/client/order/infrastructure/client/CompanyClient.java
+++ client/order/src/main/java/com/logistic/client/order/infrastructure/client/CompanyClient.java
@@ -4,17 +4,16 @@ import com.logistic.client.order.application.dto.CompanyResponse;
 import com.logistic.client.order.application.dto.OrderItemRequestDto;
 import com.logistic.client.order.application.dto.ProductPriceResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
 @FeignClient(name = "company-service")
 public interface CompanyClient {
-    @GetMapping("/api/products/stock")
-    List<ProductPriceResponse> checkAndDeductStock(List<OrderItemRequestDto> orderItems);
+    @PostMapping("/api/products/stock")
+    List<ProductPriceResponse> checkAndDeductStock(@RequestBody List<OrderItemRequestDto> orderItems);
 
     @GetMapping("/api/companies/{companyId}")
-    CompanyResponse getCompany(@PathVariable("companyId") UUID supplierCompanyId);
+    CompanyResponse getCompany(@PathVariable("companyId") UUID companyId);
 }
--- client/order/src/main/java/com/logistic/client/order/infrastructure/client/HubClient.java
+++ client/order/src/main/java/com/logistic/client/order/infrastructure/client/HubClient.java
@@ -3,6 +3,7 @@ package com.logistic.client.order.infrastructure.client;
 import com.logistic.client.order.application.dto.HubRouteResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @FeignClient(name = "hub-service")
 public interface HubClient {
 
-    @GetMapping("/api/hubs/?")
-    List<HubRouteResponse> getHubRoutes(UUID departureHubId, UUID destinationHubId);
+    @GetMapping("/api/hubs")
+    List<HubRouteResponse> getHubRoutes(@RequestParam("departureHubId") UUID departureHubId,
+                                        @RequestParam("destinationHubId") UUID destinationHubId);
 }
