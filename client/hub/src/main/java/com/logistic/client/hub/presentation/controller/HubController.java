50c15b1524778970b6c5165b4976e9a105e72813
Choi Hojin <jkhh9725@gmail.com>
2025-03-17 20:05:56 +0900

    [Feat]: HubController 생성

---
+++ client/hub/src/main/java/com/logistic/client/hub/presentation/controller/HubController.java
@@ -0,0 +1,13 @@
+package com.logistic.client.hub.presentation.controller;
+
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("api/v1/hubs")
+@AllArgsConstructor
+public class HubController {
+
+
+}
