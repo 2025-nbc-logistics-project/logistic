43d83b00d3df045f916c2aeab21c7995e9e32714
Choi Hojin <jkhh9725@gmail.com>
2025-03-17 20:19:13 +0900

    [Feat]: 허브 검색 기능 구현(LOGI-19)

--- client/hub/src/main/java/com/logistic/client/hub/domain/repository/HubRepository.java
+++ client/hub/src/main/java/com/logistic/client/hub/domain/repository/HubRepository.java
@@ -2,8 +2,9 @@ package com.logistic.client.hub.domain.repository;
 
 import com.logistic.client.hub.domain.model.Hub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface HubRepository extends JpaRepository<Hub, Long> {
+public interface HubRepository extends JpaRepository<Hub, Long> , JpaSpecificationExecutor<Hub> {
 
   boolean existsByName(String name);
 
