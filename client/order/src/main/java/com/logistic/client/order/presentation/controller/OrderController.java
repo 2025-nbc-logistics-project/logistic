efa8f93be2e1974eba4b566d6234bfa7ae945406
zapzookj <zapzook@naver.com>
2025-03-19 23:34:49 +0900

    feat : 주문 조회(단건 조회, Search) 기능 구현

--- client/order/src/main/java/com/logistic/client/order/presentation/controller/OrderController.java
+++ client/order/src/main/java/com/logistic/client/order/presentation/controller/OrderController.java
@@ -2,14 +2,14 @@ package com.logistic.client.order.presentation.controller;
 
 import com.logistic.client.order.application.dto.OrderRequestDto;
 import com.logistic.client.order.application.dto.OrderResponseDto;
+import com.logistic.client.order.application.dto.OrderSearchDto;
+import com.logistic.client.order.application.dto.PageResponseDto;
 import com.logistic.client.order.application.service.OrderApplicationService;
-import com.logistic.client.order.domain.model.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,8 +20,21 @@ public class OrderController {
 
     @PostMapping("/v1/orders")
     public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderRequestDto requestDto) {
-        Order order = orderApplicationService.createOrder(requestDto);
-        return ResponseEntity.ok(new OrderResponseDto(order));
+        OrderResponseDto responseDto = orderApplicationService.createOrder(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/v1/orders/{orderId}")
+    public ResponseEntity<OrderResponseDto> readOrder(@PathVariable("orderId") UUID orderId) {
+        OrderResponseDto responseDto = orderApplicationService.readOrder(orderId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/v1/orders/search")
+    public ResponseEntity<PageResponseDto<OrderResponseDto>> searchOrders(@ModelAttribute OrderSearchDto searchDto) {
+        searchDto.validateSize(searchDto.getSize());
+        PageResponseDto<OrderResponseDto> responseDtoPage = orderApplicationService.searchOrders(searchDto);
+        return ResponseEntity.ok(responseDtoPage);
     }
 
 }
