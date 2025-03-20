c93f7f08fa75f2704583f80cff8099480888089a
zapzookj <zapzook@naver.com>
2025-03-20 21:29:51 +0900

    refactor : 컨트롤러 RequestMapping 수정

--- client/order/src/main/java/com/logistic/client/order/presentation/controller/DeliveryController.java
+++ client/order/src/main/java/com/logistic/client/order/presentation/controller/DeliveryController.java
@@ -12,46 +12,46 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1/deliveries")
 public class DeliveryController {
 
     private final DeliveryApplicationService deliveryApplicationService;
 
-    @GetMapping("/v1/deliveries/{deliveryId}")
+    @GetMapping("/{deliveryId}")
     public ResponseEntity<DeliveryResponseDto> readDelivery(@PathVariable("deliveryId") UUID deliveryId) {
         DeliveryResponseDto responseDto = deliveryApplicationService.readDelivery(deliveryId);
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("/v1/deliveries/search")
+    @GetMapping("/search")
     public ResponseEntity<PageResponseDto<DeliverySummaryDto>> searchDeliveries(@ModelAttribute DeliverySearchDto searchDto) {
         searchDto.validateSize(searchDto.getSize());
         PageResponseDto<DeliverySummaryDto> responseDtoPage = deliveryApplicationService.searchDeliveries(searchDto);
         return ResponseEntity.ok(responseDtoPage);
     }
 
-    @PutMapping("/v1/deliveries/{deliveryId}")
+    @PutMapping("/{deliveryId}")
     public ResponseEntity<DeliverySummaryDto> updateDelivery(@PathVariable("deliveryId") UUID deliveryId,
                                                               @RequestBody DeliveryUpdateRequestDto requestDto) {
         DeliverySummaryDto responseDto = deliveryApplicationService.updateDelivery(deliveryId, requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
-    @PatchMapping("/v1/deliveries/status/{deliveryId}")
+    @PatchMapping("/status/{deliveryId}")
     public ResponseEntity<DeliverySummaryDto> updateDeliveryStatus(@PathVariable("deliveryId") UUID deliveryId,
                                                                     @RequestParam("newStatus") DeliveryStatus newStatus) {
         DeliverySummaryDto responseDto = deliveryApplicationService.updateDeliveryStatus(deliveryId, newStatus);
         return ResponseEntity.ok(responseDto);
     }
 
-    @PatchMapping("/v1/deliveries/{deliveryId}/routes/next")
+    @PatchMapping("/{deliveryId}/routes/next")
     public ResponseEntity<DeliveryRouteDto> updateNextRouteStatus(@PathVariable UUID deliveryId,
                                                                   @RequestParam("newStatus") DeliveryRouteStatus newStatus) {
         DeliveryRouteDto responseDto = deliveryApplicationService.updateNextRouteStatus(deliveryId, newStatus);
         return ResponseEntity.ok(responseDto);
     }
 
-    @PatchMapping("/v1/deliveries/{deliveryId}/routes/{routeId}")
+    @PatchMapping("/{deliveryId}/routes/{routeId}")
     public ResponseEntity<DeliveryRouteDto> updateActualDistanceTime(@PathVariable UUID deliveryId,
                                                                      @PathVariable UUID routeId,
                                                                      @RequestBody routeUpdateRequestDto requestDto) {
@@ -59,7 +59,7 @@ public class DeliveryController {
         return ResponseEntity.ok(responseDto);
     }
 
-    @PatchMapping("/v1/deliveries/{deliveryId}")
+    @PatchMapping("/{deliveryId}")
     public ResponseEntity<String> deleteDelivery(@PathVariable("deliveryId") UUID deliveryId) {
         deliveryApplicationService.deleteDelivery(deliveryId);
         return ResponseEntity.ok("삭제 성공 : " + deliveryId);
--- client/order/src/main/java/com/logistic/client/order/presentation/controller/OrderController.java
+++ client/order/src/main/java/com/logistic/client/order/presentation/controller/OrderController.java
@@ -11,43 +11,43 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1/orders")
 public class OrderController {
 
     private final OrderApplicationService orderApplicationService;
 
-    @PostMapping("/v1/orders")
+    @PostMapping("")
     public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderRequestDto requestDto) {
         OrderResponseDto responseDto = orderApplicationService.createOrder(requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("/v1/orders/{orderId}")
+    @GetMapping("/{orderId}")
     public ResponseEntity<OrderResponseDto> readOrder(@PathVariable("orderId") UUID orderId) {
         OrderResponseDto responseDto = orderApplicationService.readOrder(orderId);
         return ResponseEntity.ok(responseDto);
     }
 
-    @GetMapping("/v1/orders/search")
+    @GetMapping("/search")
     public ResponseEntity<PageResponseDto<OrderSummaryDto>> searchOrders(@ModelAttribute OrderSearchDto searchDto) {
         searchDto.validateSize(searchDto.getSize());
         PageResponseDto<OrderSummaryDto> responseDtoPage = orderApplicationService.searchOrders(searchDto);
         return ResponseEntity.ok(responseDtoPage);
     }
 
-    @PutMapping("/v1/orders/{orderId}")
+    @PutMapping("/{orderId}")
     public ResponseEntity<OrderResponseDto> updateOrder(@PathVariable("orderId") UUID orderId, @RequestBody OrderUpdateRequestDto requestDto) {
         OrderResponseDto responseDto = orderApplicationService.updateOrder(orderId, requestDto);
         return ResponseEntity.ok(responseDto);
     }
 
-    @PatchMapping("/v1/orders/status/{orderId}")
+    @PatchMapping("/status/{orderId}")
     public ResponseEntity<OrderResponseDto> updateOrderStatus(@PathVariable("orderId") UUID orderId, @RequestParam("newStatus") OrderStatus newStatus) {
         OrderResponseDto responseDto = orderApplicationService.updateOrderStatus(orderId, newStatus);
         return ResponseEntity.ok(responseDto);
     }
 
-    @PatchMapping("/v1/orders/{orderId}")
+    @PatchMapping("/{orderId}")
     public ResponseEntity<String> deleteOrder(@PathVariable("orderId") UUID orderId) {
         orderApplicationService.deleteOrder(orderId);
         return ResponseEntity.ok("삭제 성공 : " + orderId);
