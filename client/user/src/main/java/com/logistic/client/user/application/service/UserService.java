bc11b414ac8f11c496f6b9d2b125b532fb19dd64
dkki4887 <4887chaeg@gmail.com>
2025-03-22 03:03:21 +0900

    feat: 유저 서비스 개발

---
+++ client/user/src/main/java/com/logistic/client/user/application/service/UserService.java
@@ -0,0 +1,168 @@
+package com.logistic.client.user.application.service;
+
+import com.logistic.client.user.application.dto.requestDto.SignInRequestDTO;
+import com.logistic.client.user.application.dto.requestDto.UpdateUserDTO;
+import com.logistic.client.user.application.dto.requestDto.UpdateUserRoleDTO;
+import com.logistic.client.user.application.dto.requestDto.UserDTO;
+import com.logistic.client.user.application.dto.responseDto.UserResDTO;
+import com.logistic.client.user.domain.model.User;
+import com.logistic.client.user.domain.model.UserRole;
+import com.logistic.client.user.infrastructure.Security.AuthService;
+import com.logistic.client.user.infrastructure.Security.dto.CreateTokenDTO;
+import com.logistic.client.user.infrastructure.configuration.customException.AccessDeniedException;
+import com.logistic.client.user.infrastructure.configuration.customException.SamePasswordException;
+import com.logistic.client.user.infrastructure.configuration.customException.UserAlreadyExistException;
+import com.logistic.client.user.infrastructure.repository.UserRepositoryImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepositoryImpl userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthService authService;
+
+    public UserResDTO signUp(UserDTO requestUser) {
+        try {
+            if(userRepository.existsByUsernameAndIsDeletedFalse(requestUser.getUsername())) {
+                throw new UserAlreadyExistException("이미 존재하는 유저 이름입니다.");
+            }
+            //업체 & 허브 관리자의 경우 해당 업체나 허브가 존재하는지 확인 필요
+            User user = requestUser.toUser(passwordEncoder.encode(requestUser.getPassword()));
+            user.setCreatedAt(LocalDateTime.now());
+            user.setCreatedBy(requestUser.getUsername());
+            userRepository.save(user);
+
+            return UserResDTO.from(user);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public String signIn(SignInRequestDTO signInRequest) {
+        try {
+            User user = userRepository.findByUsernameAndIsDeletedFalse(signInRequest.getUsername())
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+            if (!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())){
+                throw new IllegalArgumentException("비밀번호가 올바르지 않습니다.");
+            }
+            return authService.createAccessToken(new CreateTokenDTO(user.getUsername(), user.getRole()));
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public UserResDTO getUser(String username, String signInUsername, String userRole) {
+        try {
+            UserRole role = UserRole.valueOf(userRole);
+
+            if(!role.equals(UserRole.MASTER) && !username.equals(signInUsername)) {
+                throw new AccessDeniedException("해당 유저 정보에 대해 접근 권한이 존재하지 않습니다.");
+            }
+
+            User user = userRepository.findByUsernameAndIsDeletedFalse(username)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+            return UserResDTO.from(user);
+        }
+        catch(Exception e) {
+            throw e;
+        }
+    }
+
+    //검색 및 페이징 추가 필요
+    public List<UserResDTO> getUsers(String userRole) {
+        try {
+            UserRole role = UserRole.valueOf(userRole);
+
+            if(!role.equals(UserRole.MASTER)) {
+                throw new AccessDeniedException("유저 정보에 대해 접근 권한이 존재하지 않습니다.");
+            }
+
+            List<User> users = userRepository.findAllByIsDeletedFalse();
+            return users.stream().map(UserResDTO::from).toList();
+        }
+        catch(Exception e) {
+            throw e;
+        }
+    }
+
+    @Transactional
+    public UserResDTO updateUser(String username, UpdateUserDTO requestUser, String signInUsername) {
+        try {
+            //본인 아니면 유저 정보 수정 불가
+            if(!username.equals(signInUsername)) {
+                throw new AccessDeniedException("유저 정보에 대해 수정 권한이 존재하지 않습니다.");
+            }
+            User user = userRepository.findByUsernameAndIsDeletedFalse(username)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+            if(requestUser.getPassword() != null) {
+                if(passwordEncoder.matches(requestUser.getPassword(), user.getPassword())) {
+                    throw new SamePasswordException("동일한 비밀번호로는 변경이 불가합니다.");
+                }
+                user.setPassword(passwordEncoder.encode(requestUser.getPassword()));
+            }
+            if(requestUser.getSlackId() != null) {
+                user.setSlackId(requestUser.getSlackId());
+            }
+            user.setUpdatedAt(LocalDateTime.now());
+            user.setUpdatedBy(signInUsername);
+
+            return UserResDTO.from(user);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Transactional
+    public UserResDTO updateUserRole(String username, UpdateUserRoleDTO requestDto, String userRole, String signInUsername) {
+        try {
+            UserRole role = UserRole.valueOf(userRole);
+
+            if(!role.equals(UserRole.MASTER)) {
+                throw new AccessDeniedException("유저 정보에 대해 수정 권한이 존재하지 않습니다.");
+            }
+            User user = userRepository.findByUsernameAndIsDeletedFalse(username)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+            user.setRole(requestDto.getUserRole());
+            user.setUpdatedAt(LocalDateTime.now());
+            user.setUpdatedBy(signInUsername);
+
+            return UserResDTO.from(user);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Transactional
+    public void deleteUser(String username, String userRole, String signInUsername) {
+        try {
+            UserRole role = UserRole.valueOf(userRole);
+
+            if(!role.equals(UserRole.MASTER)) {
+                throw new AccessDeniedException("유저 정보에 대해 삭제 권한이 존재하지 않습니다.");
+            }
+            User user = userRepository.findByUsernameAndIsDeletedFalse(username)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다."));
+
+            user.setDeletedBy(signInUsername);
+            user.setDeletedAt(LocalDateTime.now());
+            user.setIsDeleted(true);
+        }
+        catch (Exception e) {
+            throw e;
+        }
+    }
+}
