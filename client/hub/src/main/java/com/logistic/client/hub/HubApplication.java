f3a16a8178473abf8e35bc6dc2884d30630a12e8
Choi Hojin <jkhh9725@gmail.com>
2025-03-21 22:31:28 +0900

    [Chore]: @EnableConfigurationProperties 제거

--- client/hub/src/main/java/com/logistic/client/hub/HubApplication.java
+++ client/hub/src/main/java/com/logistic/client/hub/HubApplication.java
@@ -1,12 +1,9 @@
 package com.logistic.client.hub;
 
-import com.logistic.client.hub.infrastructure.config.NaverMapProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(NaverMapProperties.class)
 public class HubApplication {
 
 	public static void main(String[] args) {
