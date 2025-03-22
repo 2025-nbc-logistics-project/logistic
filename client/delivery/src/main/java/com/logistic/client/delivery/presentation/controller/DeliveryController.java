5ea14caa23cf499ec3313da27520d798d7a902f1
zapzookj <zapzook@naver.com>
2025-03-23 02:41:53 +0900

    feat : Delivery 도메인에 권한 검증 로직 추가

--- client/delivery/src/main/java/com/logistic/client/delivery/presentation/controller/DeliveryController.java
+++ client/delivery/src/main/java/com/logistic/client/delivery/presentation/controller/DeliveryController.java
@@ -8,6 +8,7 @@ import com.logistic.client.delivery.domain.model.DeliveryStatus;
 import com.logistic.client.delivery.presentation.request.DeliverySearchDto;
 import com.logistic.client.delivery.presentation.request.DeliveryUpdateRequestDto;
 import com.logistic.client.delivery.presentation.request.routeUpdateRequestDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,55 +23,98 @@ public class DeliveryController {
     private final DeliveryApplicationService deliveryApplicationService;
 
     @PostMapping("")
-    public FeignDeliveryResponse createDelivery(@RequestBody CreateDeliveryRequest requestDto) {
-        return deliveryApplicationService.createDelivery(requestDto);
+    public FeignDeliveryResponse createDelivery(@RequestBody CreateDeliveryRequest requestDto,
+                                                HttpServletRequest request) {
+        String role = request.getHeader("role");
+
+        return deliveryApplicationService.createDelivery(requestDto, role);
     }
 
     @GetMapping("/{deliveryId}")
-    public ResponseEntity<DeliveryResponseDto> readDelivery(@PathVariable("deliveryId") UUID deliveryId) {
-        DeliveryResponseDto responseDto = deliveryApplicationService.readDelivery(deliveryId);
+    public ResponseEntity<DeliveryResponseDto> readDelivery(@PathVariable("deliveryId") UUID deliveryId,
+                                                            HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
+        DeliveryResponseDto responseDto = deliveryApplicationService.readDelivery(deliveryId, userId, role);
         return ResponseEntity.ok(responseDto);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<PageResponseDto<DeliverySummaryDto>> searchDeliveries(@ModelAttribute DeliverySearchDto searchDto) {
+    public ResponseEntity<PageResponseDto<DeliverySummaryDto>> searchDeliveries(@ModelAttribute DeliverySearchDto searchDto,
+                                                                                HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
         searchDto.validateSize(searchDto.getSize());
-        PageResponseDto<DeliverySummaryDto> responseDtoPage = deliveryApplicationService.searchDeliveries(searchDto);
+        PageResponseDto<DeliverySummaryDto> responseDtoPage
+            = deliveryApplicationService.searchDeliveries(searchDto, userId, role);
         return ResponseEntity.ok(responseDtoPage);
     }
 
     @PutMapping("/{deliveryId}")
     public ResponseEntity<DeliverySummaryDto> updateDelivery(@PathVariable("deliveryId") UUID deliveryId,
-                                                              @RequestBody DeliveryUpdateRequestDto requestDto) {
-        DeliverySummaryDto responseDto = deliveryApplicationService.updateDelivery(deliveryId, requestDto);
+                                                             @RequestBody DeliveryUpdateRequestDto requestDto,
+                                                             HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
+        DeliverySummaryDto responseDto
+            = deliveryApplicationService.updateDelivery(deliveryId, requestDto, userId, role);
         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/status/{deliveryId}")
     public ResponseEntity<DeliverySummaryDto> updateDeliveryStatus(@PathVariable("deliveryId") UUID deliveryId,
-                                                                    @RequestParam("newStatus") DeliveryStatus newStatus) {
-        DeliverySummaryDto responseDto = deliveryApplicationService.updateDeliveryStatus(deliveryId, newStatus);
+                                                                   @RequestParam("newStatus") DeliveryStatus newStatus,
+                                                                   HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
+        DeliverySummaryDto responseDto
+            = deliveryApplicationService.updateDeliveryStatus(deliveryId, newStatus, userId, role);
         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/{deliveryId}/routes/next")
     public ResponseEntity<DeliveryRouteDto> updateNextRouteStatus(@PathVariable UUID deliveryId,
-                                                                  @RequestParam("newStatus") DeliveryRouteStatus newStatus) {
-        DeliveryRouteDto responseDto = deliveryApplicationService.updateNextRouteStatus(deliveryId, newStatus);
+                                                                  @RequestParam("newStatus") DeliveryRouteStatus newStatus,
+                                                                  HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
+        DeliveryRouteDto responseDto
+            = deliveryApplicationService.updateNextRouteStatus(deliveryId, newStatus, userId, role);
         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/{deliveryId}/routes/{routeId}")
     public ResponseEntity<DeliveryRouteDto> updateActualDistanceTime(@PathVariable UUID deliveryId,
                                                                      @PathVariable UUID routeId,
-                                                                     @RequestBody routeUpdateRequestDto requestDto) {
-        DeliveryRouteDto responseDto = deliveryApplicationService.updateActualDistanceTime(deliveryId, routeId, requestDto);
+                                                                     @RequestBody routeUpdateRequestDto requestDto,
+                                                                     HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
+        DeliveryRouteDto responseDto
+            = deliveryApplicationService.updateActualDistanceTime(deliveryId, routeId, requestDto, userId, role);
         return ResponseEntity.ok(responseDto);
     }
 
     @PatchMapping("/{deliveryId}")
-    public ResponseEntity<String> deleteDelivery(@PathVariable("deliveryId") UUID deliveryId) {
-        deliveryApplicationService.deleteDelivery(deliveryId);
+    public ResponseEntity<String> deleteDelivery(@PathVariable("deliveryId") UUID deliveryId,
+                                                 HttpServletRequest request) {
+        String userIdStr = request.getHeader("userId");
+        UUID userId = UUID.fromString(userIdStr);
+        String role = request.getHeader("role");
+
+        deliveryApplicationService.deleteDelivery(deliveryId, userId, role);
         return ResponseEntity.ok("삭제 성공 : " + deliveryId);
     }
 
