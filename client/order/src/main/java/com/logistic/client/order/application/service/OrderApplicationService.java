be75ee2cdfc8a5994dc1361181ee2a5ed9c9e1fe
zapzookj <zapzook@naver.com>
2025-03-23 01:38:21 +0900

    feat : Order 도메인에 User 데이터 추가, 권한 검증 로직 추가

--- client/order/src/main/java/com/logistic/client/order/application/service/OrderApplicationService.java
+++ client/order/src/main/java/com/logistic/client/order/application/service/OrderApplicationService.java
@@ -2,6 +2,7 @@ package com.logistic.client.order.application.service;
 
 import com.logistic.client.order.application.dto.*;
 import com.logistic.client.order.domain.exception.OrderNotFoundException;
+import com.logistic.client.order.domain.exception.UnauthorizedException;
 import com.logistic.client.order.domain.model.*;
 import com.logistic.client.order.domain.repository.OrderRepository;
 import com.logistic.client.order.domain.service.OrderDomainService;
@@ -30,7 +31,7 @@ public class OrderApplicationService {
     private final OrderDomainService orderDomainService;
 
     @Transactional
-    public OrderResponseDto createOrder(OrderRequestDto requestDto) {
+    public OrderResponseDto createOrder(OrderRequestDto requestDto, UUID userId, String username) {
         /* (1).
         입력받은 상품들의 상품 Id가 유효한 지, 재고가 충분한 지 검증,
         문제가 없다면 입력받은 수량만큼 상품 재고 차감 요청,
@@ -47,12 +48,14 @@ public class OrderApplicationService {
         List<OrderItem> orderItems =
             orderDomainService.buildOrderItems(requestDto.getOrderItems(), productPrices);
         CompanyInfo companyInfo = new CompanyInfo(supplierResponse.getCompanyId(), receiverResponse.getCompanyId());
+        Orderer orderer = new Orderer(userId, username);
 
 
         // (4). Order 엔티티 생성 및 DB 저장
         Order order = new Order(
             companyInfo,
             orderItems,
+            orderer,
             requestDto.getOrderRequest()
         );
         orderRepository.save(order);
@@ -86,33 +89,46 @@ public class OrderApplicationService {
     }
 
     @Transactional(readOnly = true)
-    public OrderResponseDto readOrder(UUID orderId) {
+    public OrderResponseDto readOrder(UUID orderId, UUID userId, String role) {
         Order order = findOrderById(orderId);
+
+        if (!canReadOrder(order, userId, role)) {
+            throw new UnauthorizedException("조회 권한이 없습니다.");
+        }
+
         return new OrderResponseDto(order);
     }
 
     @Transactional(readOnly = true)
-    public PageResponseDto<OrderSummaryDto> searchOrders(OrderSearchDto searchDto) {
+    public PageResponseDto<OrderSummaryDto> searchOrders(OrderSearchDto searchDto, UUID userId, String role) {
+        if (role.equals("DELIVERY_MANAGER") || role.equals("COMPANY_MANAGER")) {
+            searchDto.setUserId(userId);
+        }
         Page<OrderSummaryDto> mappedPage = orderRepository.searchOrders(searchDto).map(OrderSummaryDto::new);
         return new PageResponseDto<>(mappedPage);
     }
 
     @Transactional
-    public OrderResponseDto updateOrder(UUID orderId, OrderUpdateRequestDto requestDto) {
+    public OrderResponseDto updateOrder(UUID orderId, OrderUpdateRequestDto requestDto, UUID userId, String role) {
 
         // (1). 기존 Order 조회 및 수정 가능 여부 확인 (배송 중이면 X)
         Order order = findOrderById(orderId);
         order.checkEditable();
 
-        // (2). 도메인 서비스를 호출하여 기존 orderItems 와 요청 orderItems 의 차이(변경된 수량, 없어지거나 추가된 Item 등) 계산
+        // (2). 권한 검증
+        if (!canUpdateOrder(role)) {
+            throw new UnauthorizedException("수정 및 삭제 권한이 없습니다.");
+        }
+
+        // (3). 도메인 서비스를 호출하여 기존 orderItems 와 요청 orderItems 의 차이(변경된 수량, 없어지거나 추가된 Item 등) 계산
         ItemDifference diff = orderDomainService.calculateItemDifference(order.getOrderItems(), requestDto.getOrderItems());
 
-        // (3). 줄어든(없어진) 상품 재고 복원 요청
+        // (4). 줄어든(없어진) 상품 재고 복원 요청
         if (!diff.getRestoreList().isEmpty()) {
             companyClient.restoreStock(diff.getRestoreList());
         }
 
-        // (4). 늘어난(추가된) 상품 재고 차감 요청, 새로운 Price 맵에 저장
+        // (5). 늘어난(추가된) 상품 재고 차감 요청, 새로운 Price 맵에 저장
         Map<UUID, Integer> updatedPriceMap = new HashMap<>();
         if (!diff.getDeductList().isEmpty()) {
             List<ProductPriceResponse> productPrices =
@@ -124,11 +140,11 @@ public class OrderApplicationService {
         }
 
 
-        // (5). 도메인 서비스를 호출하여 최종적으로 새로운 OrderItem 목록을 재구성
+        // (6). 도메인 서비스를 호출하여 최종적으로 새로운 OrderItem 목록을 재구성
         List<OrderItem> newOrderItems
             = orderDomainService.buildUpdatedOrderItems(order.getOrderItems(), requestDto.getOrderItems(), updatedPriceMap);
 
-        // (6). Order 도메인에 최종 업데이트
+        // (7). Order 도메인에 최종 업데이트
         order.updateItems(newOrderItems);
         if (requestDto.getOrderRequest() != null) {
             order.updateRequest(requestDto.getOrderRequest());
@@ -138,7 +154,11 @@ public class OrderApplicationService {
     }
 
     @Transactional
-    public OrderResponseDto updateOrderStatus(UUID orderId, OrderStatus newStatus) {
+    public OrderResponseDto updateOrderStatus(UUID orderId, OrderStatus newStatus, UUID userId, String role) {
+        if (!canUpdateOrder(role)) {
+            throw new UnauthorizedException("수정 및 삭제 권한이 없습니다.");
+        }
+
         Order order = findOrderById(orderId);
 
         order.updateStatus(newStatus);
@@ -147,9 +167,13 @@ public class OrderApplicationService {
     }
 
     @Transactional
-    public void deleteOrder(UUID orderId) {
+    public void deleteOrder(UUID orderId, UUID userId, String role) {
         Order order = findOrderById(orderId);
         order.checkEditable();
+
+        if (!canUpdateOrder(role)) {
+            throw new UnauthorizedException("수정 및 삭제 권한이 없습니다.");
+        }
 
         List<OrderItemRequestDto> restoreList = order.getOrderItems().stream()
                 .map(oi -> new OrderItemRequestDto(
@@ -165,11 +189,31 @@ public class OrderApplicationService {
         if (order.getDeliveryId() != null) {
             deliveryClient.deleteDelivery(order.getDeliveryId());
         }
-        order.markAsDeleted(1L); // TODO : 실제 유저 Id 추가
+        order.markAsDeleted(userId);
     }
 
     private Order findOrderById(UUID orderId) {
         return orderRepository.findById(orderId)
             .orElseThrow(() -> new OrderNotFoundException("해당 Id를 가진 주문 정보를 찾지 못했습니다."));
+    }
+
+    private boolean canReadOrder(Order order, UUID userId, String role) {
+        if (role.equals("MASTER")) return true;
+
+        if (role.equals("HUB_MANAGER")) { // 허브 매니저의 권한 검증은 조금 생각이 필요할듯
+            return true; // 일단 임시로 true
+        }
+
+        if (role.equals("DELIVERY_MANAGER") || role.equals("COMPANY_MANAGER")) {
+            return order.getOrderer() != null && order.getOrderer().getUserId().equals(userId);
+        }
+
+        return false;
+    }
+
+    private boolean canUpdateOrder(String role) {
+        if (role.equals("MASTER")) return true;
+
+        return role.equals("HUB_MANAGER");
     }
 }
