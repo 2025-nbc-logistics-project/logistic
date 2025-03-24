a3b6b4efe072d982641c2688839fd58e4b4532af
Choi Hojin <jkhh9725@gmail.com>
2025-03-25 01:41:20 +0900

    [Chore]: 불필요한 속성 무시

--- client/hub/src/main/java/com/logistic/client/hub/application/dto/UserResponseDto.java
+++ client/hub/src/main/java/com/logistic/client/hub/application/dto/UserResponseDto.java
@@ -1,8 +1,9 @@
 package com.logistic.client.hub.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.UUID;
 import lombok.Getter;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 public class UserResponseDto {
   private String username;
