b7fe0dcb3540dc37de80ccbb4830fdab9696c3ae
Choi Hojin <jkhh9725@gmail.com>
2025-03-24 10:38:27 +0900

    [Feat]: cacheManager cache 정책 분리

--- client/hub/src/main/java/com/logistic/client/hub/infrastructure/config/CacheConfig.java
+++ client/hub/src/main/java/com/logistic/client/hub/infrastructure/config/CacheConfig.java
@@ -1,12 +1,16 @@
 package com.logistic.client.hub.infrastructure.config;
 
-
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
@@ -17,6 +21,26 @@ public class CacheConfig {
   @Bean
   public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
 
+    RedisCacheConfiguration hubCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+        .disableCachingNullValues()
+        .entryTtl(Duration.ofHours(1))
+        .computePrefixWith(CacheKeyPrefix.simple())
+        .serializeValuesWith(SerializationPair.fromSerializer(RedisSerializer.json()));
+
+    RedisCacheConfiguration allHubsCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+        .disableCachingNullValues()
+        .entryTtl(Duration.ofHours(2))
+        .computePrefixWith(CacheKeyPrefix.simple())
+        .serializeValuesWith(SerializationPair.fromSerializer(RedisSerializer.json()));
+
+    Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+    cacheConfigurations.put("hubCache", hubCacheConfig);
+    cacheConfigurations.put("allHubsCache", allHubsCacheConfig);
+
+    return RedisCacheManager.builder(redisConnectionFactory)
+        .cacheDefaults(hubCacheConfig)
+        .withInitialCacheConfigurations(cacheConfigurations)
+        .build();
   }
 
 }
